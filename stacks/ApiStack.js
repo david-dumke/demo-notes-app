99919ed597322dd7037e16c8076a96f355e6493e
David Dumke <ddumke@orderkangaroo.com>
2022-03-08 09:13:09 -0400

    Add Stripe billing

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -24,6 +24,7 @@ export default class ApiStack extends sst.Stack {
                 "PUT    /notes/{id}": "src/note/update.main",
                 "GET    /notes/{id}": "src/note/get.main",
                 "DELETE /notes/{id}": "src/note/delete.main",
+                "POST   /billing": "src/util/billing.main",
             }
         });
 
